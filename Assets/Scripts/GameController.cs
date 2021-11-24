5b130d27e2173dece46ab24f48008f65331f8c7f
Egemen <68378905+AthenaSenior@users.noreply.github.com>
2021-11-24 23:39:48 +0300

    Update GameController.cs
    Egemen

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,8 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    //Þu an sadece karakter hareketine odaklanalým ben ölüm durumlarýný ve tekrar baþlatma fonksiyonlarýný ayarlayacaðým sonra üzerine çalýþýrýz.
+    public Rigidbody2D rb;
+    public SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        getComponents();
+    }
+
+    private void getComponents()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            changeGravity();
+            changeSprite();
+        }
+    }
+
+    private void changeSprite()
+    {
+     spriteRenderer.flipY = rb.gravityScale < 0;
+    }
+
+    private void changeGravity()
+    {
+       rb.gravityScale *= -1;
+    }
+
 }
